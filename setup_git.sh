a1089933e3db0e05dbf15ffae891fb1d1bf87790
doliv <61095881+doliv8@users.noreply.github.com>
2025-06-20 17:22:55 +0200

    started pwn setup

---
+++ setup_git.sh
@@ -0,0 +1,2 @@
+git config --global user.email "61095881+doliv8@users.noreply.github.com"
+git config --global user.name "doliv"
---
+++ setup_pwn.sh
@@ -0,0 +1,3 @@
+pip3 install pwntools --break-system-packages
+pip install git+https://github.com/Church-17/pwnit.git --break-system-packages
+
