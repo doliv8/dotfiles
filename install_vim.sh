4f1f38800463e00a3441e26d59bb3c142010bb89
doliv <61095881+doliv8@users.noreply.github.com>
2025-06-20 17:14:57 +0200

    updated vim install

--- install_vim.sh
+++ install_vim.sh
@@ -1,4 +1,7 @@
 sudo apt install vim-gtk3
+if [ -v WAYLAND_DISPLAY ]; then
+	sudo apt install wl-clipboard
+fi
 
 # install vundle
 git clone --depth=1 https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
