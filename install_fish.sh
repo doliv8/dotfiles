1b72c84fd460b58d172cc79759bd55e706953896
doliv <61095881+doliv8@users.noreply.github.com>
2025-06-20 16:14:17 +0200

    updated fish install

--- install_fish.sh
+++ install_fish.sh
@@ -4,14 +4,14 @@ sudo -u root chsh -s /usr/bin/fish
 chsh -s /usr/bin/fish
 
 # create user conf
-if [[ -d "~/.config/fish" ]]; then
+if [ -d "~/.config/fish" ]; then
 	mv ~/.config/fish ~/.config/fish~
 	echo "Backup of existing fish config has been created"
 fi
 ln -sf $(pwd)/fish/ ~/.config/
 
 # create root conf
-if [[ -d "/root/.config/fish" ]]; then
+if [ -d "/root/.config/fish" ]; then
 	sudo mv /root/.config/fish /root/.config/fish~
 	echo "Backup of existing root fish config has been created"
 fi
