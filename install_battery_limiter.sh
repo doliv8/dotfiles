460deab6a826982f1651504079cb27cc4cb8f710
doliv <61095881+doliv8@users.noreply.github.com>
2026-01-23 12:09:27 +0100

    updated battery charge limiter

--- install_battery_limiter.sh
+++ install_battery_limiter.sh
@@ -1,3 +1,4 @@
+# must have updated huawei-wmi driver to work on HUAWEI Matebook (needs smart_charge support)
 sudo cp battery_limiter/battery_charge_threshold.service /etc/systemd/system/battery_charge_threshold.service
 sudo cp battery_limiter/battery_charge_threshold.timer /etc/systemd/system/battery_charge_threshold.timer
 sudo systemctl daemon-reload
